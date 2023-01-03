053138bc98a0c9a6df9e1c85880dd98ab2444e67
godmonera <borwonpak_du@cmu.ac.th>
2023-01-03 16:30:03 +0700

    finished

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include<fstream>
+#include <iomanip>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    double sum ,sumpow2, i=0;
+    ifstream source;
+    source.open("score.txt");
+
+    string textline;
+	while (getline( source , textline )) { 
+    
+		sum = sum + atof(textline.c_str());
+        sumpow2 = sumpow2 + pow(atof(textline.c_str()),2);
+        i++;
+
+		} 
+
+   
+    source.close();
+    double mean , sd;
+    mean = sum/i;   
+    sd = sqrt(sumpow2/i - pow(mean,2));       
+
+    cout << "Number of data = "<< i <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean <<"\n";
+    cout << "Standard deviation = "<< sd <<"\n";
+
+}
